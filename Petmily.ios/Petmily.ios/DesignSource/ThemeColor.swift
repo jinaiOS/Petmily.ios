878589b006ab0a7e21b64f49e27ccb96e08b8b76
jiwook_han <jiwook.han.dev@gmail.com>
2024-02-16 00:31:17 +0900

    [DESIGN] : 🎨 hexString 값의 색상을 변환하는 코드 및 ThemeColor 추가

---
+++ Petmily.ios/Petmily.ios/DesignSource/ThemeColor.swift
@@ -0,0 +1,12 @@
+//
+//  ThemeColor.swift
+//  Petmily.ios
+//
+//  Copyright (c) 2024 z-wook. All right reserved.
+//
+
+import UIKit
+
+enum ThemeColor {
+    static let pink = UIColor(hexString: "FFB6C7")
+}
