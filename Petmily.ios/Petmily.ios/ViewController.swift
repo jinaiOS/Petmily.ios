d34be7eba8fc83d528abbb0e635112b74484e9c9
jinaiOS <kj227777@naver.com>
2024-02-15 23:48:11 +0900

    Xcode init

---
+++ Petmily.ios/Petmily.ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petmily.ios
+//
+//  Created by 김지은 on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
