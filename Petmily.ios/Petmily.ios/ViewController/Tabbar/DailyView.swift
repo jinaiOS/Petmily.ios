bc191912e9d3f87ef71e9df982a87c4ce4a7ae11
jina kim <jina.kim@devercorp.com>
2024-02-24 12:54:40 +0900

    [FEAT]: 데일리 화면 layout 구성 완료

--- Petmily.ios/Petmily.ios/ViewController/Tabbar/DailyView.swift
+++ Petmily.ios/Petmily.ios/ViewController/Tabbar/DailyView.swift
@@ -79,6 +79,95 @@ class DailyView: UIView {
         button.setImage(UIImage(named: "btn_more"), for: .normal)
         return button
     }()
+    /** @brief 내용 stackview */
+    lazy var stvContent: UIStackView = {
+       let stackview = UIStackView()
+        stackview.axis = .vertical
+        [vProfile, vLocationTag].forEach { stackview.addArrangedSubview($0) }
+        return stackview
+    }()
+    /** @brief 프로필 view */
+    lazy var vProfile: UIView = {
+       let view = UIView()
+        [imgProfile, lblProfile, lblPetName, lblPetType, lblContent].forEach { view.addSubview($0) }
+        return view
+    }()
+    /** @brief 프로필 이미지 imageview */
+    lazy var imgProfile: UIImageView = {
+       let imageview = UIImageView()
+        imageview.cornerRadius = imageview.frame.width
+        return imageview
+    }()
+    /** @brief 사용자 이름 label */
+    lazy var lblProfile: UILabel = {
+       let label = UILabel()
+        label.text = "상우네"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white
+        return label
+    }()
+    /** @brief 펫 이름 label */
+    lazy var lblPetName: UILabel = {
+       let label = UILabel()
+        label.text = "단지"
+        label.font = .boldSystemFont(ofSize: 20)
+        label.textColor = .primary
+        return label
+    }()
+    /** @brief 펫 종류 label */
+    lazy var lblPetType: UILabel = {
+       let label = UILabel()
+        label.text = "멍뭉이"
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .white
+        return label
+    }()
+    /** @brief 내용 label */
+    lazy var lblContent: UILabel = {
+       let label = UILabel()
+        label.text = "이쁘죠? 귀엽죠? 못 참겠죠?"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white
+        return label
+    }()
+    /** @brief 위치, 해시태그 view */
+    lazy var vLocationTag: UIView = {
+       let view = UIView()
+        [vLocation, vTag].forEach { view.addSubview($0) }
+        return view
+    }()
+    /** @brief 위치 view */
+    lazy var vLocation: UIView = {
+       let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.8509803922, green: 0.8509803922, blue: 0.8509803922, alpha: 1)
+        view.addSubview(lblLocation)
+        view.cornerRadius = 11
+        return view
+    }()
+    /** @brief 위치 label */
+    lazy var lblLocation: UILabel = {
+       let label = UILabel()
+        label.text = "위치 - 부천"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = #colorLiteral(red: 0.6431372549, green: 0.6431372549, blue: 0.6431372549, alpha: 1)
+        return label
+    }()
+    /** @brief 해시태그 view */
+    lazy var vTag: UIView = {
+       let view = UIView()
+        view.backgroundColor = .primary
+        view.addSubview(lblTag)
+        view.cornerRadius = 11
+        return view
+    }()
+    /** @brief 해시태그 label */
+    lazy var lblTag: UILabel = {
+       let label = UILabel()
+        label.text = "#태그"
+        label.font = .systemFont(ofSize: 18)
+        label.textColor = .white
+        return label
+    }()
     
     init() {
         super.init(frame: .zero)
@@ -90,17 +179,27 @@ class DailyView: UIView {
     }
     
     private func setLayout() {
-        [cvMain, lblHeader, stvButtons].forEach { addSubview($0) }
+        [cvMain, lblHeader, stvButtons, stvContent].forEach { addSubview($0) }
         
-        if Common.hasSafeArea {
+        if !Common.IS_IPHONE_SE() {
             lblHeader.snp.makeConstraints {
                 $0.top.equalToSuperview().inset(51)
                 $0.leading.equalToSuperview().inset(15)
+            }
+            
+            stvContent.snp.makeConstraints {
+                $0.leading.equalToSuperview().inset(15)
+                $0.bottom.equalToSuperview().inset(145)
             }
         } else {
             lblHeader.snp.makeConstraints {
                 $0.top.equalToSuperview().inset(31)
                 $0.leading.equalToSuperview().inset(15)
+            }
+            
+            stvContent.snp.makeConstraints {
+                $0.leading.equalToSuperview().inset(15)
+                $0.bottom.equalToSuperview().inset(125)
             }
         }
         
@@ -116,6 +215,55 @@ class DailyView: UIView {
             $0.width.equalTo(40)
             $0.trailing.equalToSuperview().inset(25)
             $0.bottom.equalTo(safeAreaLayoutGuide).inset(160)
+        }
+        
+        vProfile.snp.makeConstraints {
+            $0.height.equalTo(98)
+        }
+        
+        imgProfile.snp.makeConstraints {
+            $0.top.leading.equalToSuperview()
+            $0.width.height.equalTo(50)
+        }
+        
+        lblProfile.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(4)
+            $0.leading.equalTo(imgProfile.snp.trailing).offset(16)
+            $0.trailing.equalToSuperview().inset(10)
+        }
+        
+        lblPetName.snp.makeConstraints {
+            $0.top.equalTo(lblProfile.snp.bottom).offset(2)
+            $0.leading.equalTo(imgProfile.snp.trailing).offset(16)
+        }
+        
+        lblPetType.snp.makeConstraints {
+            $0.leading.equalTo(lblPetName.snp.trailing).offset(2)
+            $0.bottom.equalTo(lblPetName)
+        }
+        
+        lblContent.snp.makeConstraints {
+            $0.top.equalTo(imgProfile.snp.bottom).offset(20)
+            $0.leading.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+        
+        vLocation.snp.makeConstraints {
+            $0.height.equalTo(32)
+            $0.top.bottom.leading.equalToSuperview()
+        }
+        
+        lblLocation.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(6)
+        }
+        
+        vTag.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(vLocation.snp.trailing).offset(6)
+        }
+        
+        lblTag.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(6)
         }
     }
 }
