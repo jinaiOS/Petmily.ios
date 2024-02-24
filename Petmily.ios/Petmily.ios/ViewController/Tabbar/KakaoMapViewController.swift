f0b27523107aad4e2a07b737af97b683eb4280b7
DanjyChichi <td0413@daum.net>
2024-02-24 18:06:42 +0900

    [PACK] : 🎁 KakaoMap SDK 패키지 추가

--- Petmily.ios/Petmily.ios/ViewController/Tabbar/KakaoMapViewController.swift
+++ Petmily.ios/Petmily.ios/ViewController/Tabbar/KakaoMapViewController.swift
@@ -1,8 +1,128 @@
-//
-//  KakaoMapViewController.swift
-//  Petmily.ios
-//
-//  Created by 박상우 on 2/23/24.
-//
+import KakaoMapsSDK
+import UIKit
 
-import Foundation
+class KakaoMapViewController: UIViewController, MapControllerDelegate {
+    // 지도 컨트롤러 및 뷰 컨테이너 선언
+    var mapController: KMController?
+    var mapContainer: KMViewContainer?
+    // 옵저버 추가 여부 및 인증 상태 플래그
+    var _observerAdded: Bool = false
+    var _auth: Bool = false
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        
+        // KakaoMapsSDK 초기화 및 지도 컨트롤러와 뷰 컨테이너 설정
+        mapContainer = KMViewContainer()
+        mapController = KMController(viewContainer: mapContainer!)
+        mapController!.delegate = self
+        mapController?.initEngine() // 엔진 초기화
+        mapController?.authenticate() // 인증 요청
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        // 객체 소멸 시 지도 렌더링 및 엔진 중지
+        mapController?.stopRendering()
+        mapController?.stopEngine()
+        print("deinit")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupMap() // 지도 설정
+        setupUI() // UI 설정
+    }
+
+    func setupMap() {
+        mapContainer = KMViewContainer()
+        mapController = KMController(viewContainer: mapContainer!)
+        mapController!.delegate = self
+        mapController?.initEngine() // 엔진 초기화
+        mapController?.authenticate() // 인증 요청
+    }
+    
+    func setupUI() {
+        view.addSubview(mapContainer!)
+        mapContainer!.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    /**
+     @brief 지도에 뷰 추가
+     */
+    func addViews() {
+        let defaultPosition: MapPoint = MapPoint(longitude: 127.108678, latitude: 37.402001)
+        let mapviewInfo: MapviewInfo = MapviewInfo(viewName: "mapview", appName: "openmap", viewInfoName: "map", defaultPosition: defaultPosition, defaultLevel: 7)
+
+        if mapController?.addView(mapviewInfo) == Result.OK {
+            print("OK")
+        }
+    }
+    
+    /**
+     @brief 컨테이너 크기 조정 시 호출되는 메서드
+     
+     @param size 컨테이너 사이즈
+     */
+    func containerDidResized(_ size: CGSize) {
+        let mapView: KakaoMap? = mapController?.getView("mapview") as? KakaoMap
+        // 나침반 위치 및 이벤트 핸들러 설정
+        mapView?.setCompassPosition(origin: GuiAlignment(vAlign: .bottom, hAlign: .left), position: CGPoint(x: 10.0, y: 150.0))
+        mapView?.addCompassTappedEventHandler(target: self, handler: KakaoMapViewController.compassTappedHanlder)
+        mapView?.showCompass()
+        mapView?.viewRect = CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: size)
+    }
+
+    /**
+     @brief 뷰가 소멸될 때 호출되는 메서드
+     
+     @param view View 객체
+     */
+    func viewWillDestroyed(_ view: ViewBase) {
+
+    }
+    
+    /**
+     @brief 앱의 활성 상태 변경 시 옵저버 추가/제거
+     */
+    func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(willResignActive), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: UIApplication.didBecomeActiveNotification, object: nil)
+        _observerAdded = true
+    }
+
+    func removeObservers() {
+        NotificationCenter.default.removeObserver(self, name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIApplication.didBecomeActiveNotification, object: nil)
+        _observerAdded = false
+    }
+    
+    /**
+     @brief 나침반 탭 이벤트 핸들러
+     
+     @param 카카오맵 객체
+     */
+    func compassTappedHanlder(_ kakaoMap: KakaoMap) {
+        print("compass tapped")
+        kakaoMap.resetCameraOrientation() // 카메라 방향 리셋
+    }
+
+    /**
+     @brief 앱이 비활성화 될 떄 호출되는 메서드
+     */
+    @objc func willResignActive() {
+        mapController?.stopRendering()
+    }
+
+    /**
+     @brief 앱이 다시 활성 상태가 될 떄 호출되는 메서드
+     */
+    @objc func didBecomeActive() {
+        mapController?.startRendering()
+    }
+}
--- Petmily.ios/Petmily.ios/ViewController/Tabbar/LocationViewController.swift
+++ Petmily.ios/Petmily.ios/ViewController/Tabbar/LocationViewController.swift
@@ -1,29 +1,73 @@
-//
-//  LocationViewController.swift
-//  Petmily.ios
-//
-//  Created by 박상우 on 2/16/24.
-//
-
 import UIKit
+import SnapKit
 
 class LocationViewController: UIViewController {
+    let kakaoMapViewController = KakaoMapViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+        addChild(kakaoMapViewController)
+        view.addSubview(kakaoMapViewController.view)
+        kakaoMapViewController.view.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        // 자식 뷰 컨트롤러의 이동이 완료되었음을 알림
+        kakaoMapViewController.didMove(toParent: self)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 앱의 활성 상태에 따라 옵저버 추가 및 지도 렌더링 상태 확인
+        kakaoMapViewController.addObservers()
+        if kakaoMapViewController._auth {
+            // 엔진이 시작되지 않았다면 시작
+            if kakaoMapViewController.mapController?.engineStarted == false {
+                kakaoMapViewController.mapController?.startEngine()
+            }
+
+            // 렌더링이 시작되지 않았다면 시작
+            if kakaoMapViewController.mapController?.rendering == false {
+                kakaoMapViewController.mapController?.startRendering()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /**
+     @brief 뷰의 서브뷰들이 레이아웃될 때 호출되는 메서드
+     
+     뷰의 크기가 변경될 때 KakaoMapViewController의 지도 컨테이너 크기도 조정
+     */
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        kakaoMapViewController.containerDidResized(view.bounds.size)
     }
-    */
 
+    /**
+     @brief 사용자 인증이 성공했을 때 호출되는 메서드
+     
+     인증 성공 후 지도 엔진 시작 및 렌더링을 시작하고 지도 뷰를 추가
+     */
+    func authenticationSucceeded() {
+        kakaoMapViewController._auth = true
+        kakaoMapViewController.mapController?.startEngine()
+        kakaoMapViewController.mapController?.startRendering()
+        kakaoMapViewController.addViews()
+    }
+
+    /**
+     @brief 사용자 인증이 실패했을 때 호출되는 메서드
+     
+     @param errorCode 인증 실패 에러 코드
+     @param desc 인증 실패 상세 설명
+     인증 실패 후 로그 출력 및 5초 후 재인증 시도
+     */
+    func authenticationFailed(_ errorCode: Int, desc: String) {
+        print("error code: \(errorCode)")
+        print("\(desc)")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            print("retry auth...")
+            self.kakaoMapViewController.mapController?.authenticate()
+        }
+    }
 }
