b9b022d11254a0b4570c40ba92e500dd06163ad8
jiwook_han <jiwook.han.dev@gmail.com>
2024-03-06 04:36:46 +0900

    [FIX] : 🔧 ShareInfo 모델 변경

--- Petmily.ios/Petmily.ios/ViewController/Tabbar/Info/InfoRoot/Model/ShareInfo.swift
+++ Petmily.ios/Petmily.ios/ViewController/Tabbar/Info/InfoRoot/Model/ShareInfo.swift
@@ -8,13 +8,28 @@
 import Foundation
 
 struct ShareInfo: Codable, Hashable {
-    let shareID: UUID
+    let shareID: UUID // 고유 번호 자동 생성
     let title: String
     let content: String
     let author: String
-    let hashtag: String
+    let hashtag: [String]
     let profileUrl: String
     let contentImageUrl: String
-    var createTime = Date() // Firestore에 Create(작성) 되는 시각이므로 따로 시각 설정하지 않기
-    var like: [String] = []
+    let createTime: Date // Firestore에 Create(작성) 되는 시각이므로 따로 시각 설정하지 않기
+    var like: [String]
+    var likeCount: Int
+    
+    init(title: String, content: String, author: String, hashtag: [String],
+         profileUrl: String, contentImageUrl: String) {
+        self.shareID = UUID()
+        self.title = title
+        self.content = content
+        self.author = author
+        self.hashtag = hashtag
+        self.profileUrl = profileUrl
+        self.contentImageUrl = contentImageUrl
+        self.createTime = Date()
+        self.like = []
+        self.likeCount = 0
+    }
 }
