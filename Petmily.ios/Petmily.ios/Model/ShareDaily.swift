5504dd59ab3a16df818611ba7ca5526018781268
jina kim <jina.kim@devercorp.com>
2024-03-17 10:20:30 +0900

    [FEAT]: 영상 고르면 재생하도록 구현 / infoviewmodel에서 url 변환해서 firebase에 올림

--- Petmily.ios/Petmily.ios/Model/ShareDaily.swift
+++ Petmily.ios/Petmily.ios/Model/ShareDaily.swift
@@ -7,14 +7,28 @@
 
 import Foundation
 
-struct ShareDaily: Hashable {
-    let postID: UUID
+struct ShareDaily: Codable, Hashable {
+    let shareID: UUID // 고유 번호 자동 생성
+    let title: String
     let content: String
-    let userID: String
-    let contentVideoUrl: String
-    let hashtag: String
-    let date: Date
-    let like: [String]
-    let location: String
-    let petID: [String]?
+    let author: String
+    let hashtag: [String]
+    let profileUrl: String
+    var contentVideoUrl: URL
+    let createTime: Date // Firestore에 Create(작성) 되는 시각이므로 따로 시각 설정하지 않기
+    var like: [String]
+    var likeCount: Int
+    
+    init(title: String, content: String, author: String, hashtag: [String], profileUrl: String, contentVideoUrl: URL) {
+        self.shareID = UUID()
+        self.title = title
+        self.content = content
+        self.author = author
+        self.hashtag = hashtag
+        self.profileUrl = profileUrl
+        self.contentVideoUrl = contentVideoUrl
+        self.createTime = Date()
+        self.like = []
+        self.likeCount = 0
+    }
 }
