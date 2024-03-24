50ae1d033fdf7272043f702fec7f70857c2e6cea
DanjyChichi <td0413@daum.net>
2024-03-24 12:24:12 +0900

    [FIX] : Constants 수정

--- Petmily.ios/Petmily.ios/Util/Constants.swift
+++ Petmily.ios/Petmily.ios/Util/Constants.swift
@@ -17,9 +17,7 @@ extension Constants {
 
 extension Constants {
     enum Radius {
-        static let radius2: CGFloat = 2
         static let radius5: CGFloat = 5
-        static let radius6: CGFloat = 6
         static let radius7: CGFloat = 7
         static let radius9: CGFloat = 9
         
@@ -30,24 +28,18 @@ extension Constants {
     enum Spacing {
         static let spacing1: CGFloat = 1
         static let spacing2: CGFloat = 2
-        static let spacing3: CGFloat = 3
         static let spacing4: CGFloat = 4
         static let spacing5: CGFloat = 5
         static let spacing6: CGFloat = 6
         static let spacing8: CGFloat = 8
         
         static let spacing10: CGFloat = 10
-        static let spacing11: CGFloat = 11
-        static let spacing12: CGFloat = 12
         static let spacing14: CGFloat = 14
         static let spacing16: CGFloat = 16
         
         static let spacing20: CGFloat = 20
-        static let spacing24: CGFloat = 24
-        static let spacing29: CGFloat = 29
         
         static let spacing40: CGFloat = 40
-        static let spacing41: CGFloat = 41
         static let spacing160: CGFloat = 160
     }
 }
@@ -63,10 +55,8 @@ extension Constants {
         
         static let size10: CGFloat = 10
         static let size12: CGFloat = 12
-        static let size13: CGFloat = 13
         static let size14: CGFloat = 14
         static let size16: CGFloat = 16
-        static let size19: CGFloat = 19
         
         static let size20: CGFloat = 20
         static let size22: CGFloat = 22
@@ -74,35 +64,20 @@ extension Constants {
         static let size26: CGFloat = 26
         
         static let size30: CGFloat = 30
-        static let size33: CGFloat = 33
-        static let size36: CGFloat = 36
         
         static let size40: CGFloat = 40
         static let size41: CGFloat = 41
-        static let size42: CGFloat = 42
         static let size45: CGFloat = 45
-        static let size46: CGFloat = 46
-        static let size49: CGFloat = 49
         
         static let size50: CGFloat = 50
         static let size52: CGFloat = 52
-        
-        static let size75: CGFloat = 75
-        
+                
         static let size80: CGFloat = 80
-        
-        static let size99: CGFloat = 99
-        
+                
         static let size100: CGFloat = 100
-        
-        static let size114: CGFloat = 114
-        
-        static let size121: CGFloat = 121
-        
+                        
         static let size146: CGFloat = 146
         
         static let size152: CGFloat = 152
-        
-        static let size182: CGFloat = 182
     }
 }
